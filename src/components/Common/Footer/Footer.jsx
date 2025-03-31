78406d05e0da9043f6317e025037aff83e8bdcda
LEKM <83527522+als229@users.noreply.github.com>
2025-03-31 22:23:05 +0900

    저장 안된애들

--- src/components/Common/Footer/Footer.jsx
+++ src/components/Common/Footer/Footer.jsx
@@ -1,14 +1,10 @@
 import {} from "./Footer.styles";
-import { AiOutlineHome } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <>
       <footer>
-        <h1>
-          <AiOutlineHome />
-          멍멍아
-        </h1>
+        <h1>멍멍아</h1>
       </footer>
     </>
   );
